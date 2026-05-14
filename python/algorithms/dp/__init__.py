54e8579bfa1c600f735e59a65cebc0afd1d6aaae
souradeepta <sdb.svnit@gmail.com>
2026-05-14 12:43:35 -0700

    feat: update algorithm module exports for new implementations
    Changes:
    - dp/__init__.py: Add 15 new functions (8 backtracking + 7 grid DP)
    - graph/__init__.py: Add 14 new items (TreeNode + 8 tree DP + 6 traversals)
    - All 30 new algorithms now importable from their modules

--- python/algorithms/dp/__init__.py
+++ python/algorithms/dp/__init__.py
@@ -1,5 +1,6 @@
 """Dynamic Programming algorithms package."""
 from .dp import (
+    # Classic DP
     fibonacci,
     knapsack_01,
     lcs,
@@ -8,9 +9,27 @@ from .dp import (
     coin_change,
     matrix_chain_mult,
     longest_palindromic_substr,
+    # Backtracking
+    solve_nqueens,
+    solve_sudoku,
+    word_search,
+    permute,
+    combine,
+    letter_combinations,
+    subsets,
+    generate_parentheses,
+    # Grid & 2D DP
+    unique_paths,
+    bomb_enemy,
+    max_island_area,
+    dungeon_game,
+    trapping_rain_water_2d,
+    word_ladder,
+    word_pattern_match,
 )
 
 __all__ = [
+    # Classic DP
     "fibonacci",
     "knapsack_01",
     "lcs",
@@ -19,4 +38,21 @@ __all__ = [
     "coin_change",
     "matrix_chain_mult",
     "longest_palindromic_substr",
+    # Backtracking
+    "solve_nqueens",
+    "solve_sudoku",
+    "word_search",
+    "permute",
+    "combine",
+    "letter_combinations",
+    "subsets",
+    "generate_parentheses",
+    # Grid & 2D DP
+    "unique_paths",
+    "bomb_enemy",
+    "max_island_area",
+    "dungeon_game",
+    "trapping_rain_water_2d",
+    "word_ladder",
+    "word_pattern_match",
 ]
--- python/algorithms/graph/__init__.py
+++ python/algorithms/graph/__init__.py
@@ -1,5 +1,6 @@
 """Graph algorithms package."""
 from .graph_algorithms import (
+    # Classic graph algorithms
     dijkstra,
     bellman_ford,
     floyd_warshall,
@@ -8,9 +9,25 @@ from .graph_algorithms import (
     tarjan_scc,
     topological_sort_kahn,
     astar,
+    # Tree structures and algorithms
+    TreeNode,
+    lca,
+    path_sum,
+    all_paths_sum,
+    tree_diameter,
+    rob_tree,
+    build_tree_preorder_inorder,
+    serialize_tree,
+    deserialize_tree,
+    # Graph traversals
+    count_islands,
+    is_bipartite,
+    has_cycle_directed,
+    has_cycle_undirected,
 )
 
 __all__ = [
+    # Classic graph algorithms
     "dijkstra",
     "bellman_ford",
     "floyd_warshall",
@@ -19,4 +36,19 @@ __all__ = [
     "tarjan_scc",
     "topological_sort_kahn",
     "astar",
+    # Tree structures and algorithms
+    "TreeNode",
+    "lca",
+    "path_sum",
+    "all_paths_sum",
+    "tree_diameter",
+    "rob_tree",
+    "build_tree_preorder_inorder",
+    "serialize_tree",
+    "deserialize_tree",
+    # Graph traversals
+    "count_islands",
+    "is_bipartite",
+    "has_cycle_directed",
+    "has_cycle_undirected",
 ]
